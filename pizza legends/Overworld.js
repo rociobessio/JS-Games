e269f96d41d2532676d01e2e6447689ade41cda2
rociobessio <rocibessio@gmail.com>
2022-07-08 00:32:39 -0300

    cutscenes & behaviours part 1

--- pizza legends/Overworld.js
+++ pizza legends/Overworld.js
@@ -26,7 +26,9 @@ class Overworld {
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
       //Draw Game Objects
-      Object.values(this.map.gameObjects).forEach(object => {
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
         object.sprite.draw(this.ctx, cameraPerson);
       })
 
@@ -48,5 +50,14 @@ class Overworld {
   this.directionInput.init();
 
   this.startGameLoop();
+
+  this.map.startCutscene([
+    { who: "hero", type: "walk",  direction: "down" },
+    { who: "hero", type: "walk",  direction: "down" },
+    { who: "npcA", type: "walk",  direction: "left" },
+    { who: "npcA", type: "walk",  direction: "left" },
+    { who: "npcA", type: "stand",  direction: "up", time: 800 },
+  ])
+
  }
 }
--- pizza legends/utils.js
+++ pizza legends/utils.js
@@ -2,9 +2,11 @@ const utils = {
   withGrid(n) {
     return n * 16;
   },
+
   asGridCoord(x,y) {
     return `${x*16},${y*16}`
   },
+
   nextPosition(initialX, initialY, direction) {
     let x = initialX;
     let y = initialY;
@@ -19,6 +21,13 @@ const utils = {
       y += size;
     }
     return {x,y};
+  },
+
+  emitEvent(name,detail){
+    const event = new CustomEvent(name,{
+      detail
+    });
+    document.dispatchEvent(event);
+
   }
-  
-}
+  }
